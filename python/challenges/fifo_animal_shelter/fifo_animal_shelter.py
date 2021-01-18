fb518c636122562f52bb377f0afc6e1f5a902e1e
louiscaruso <carusolouis00@gmail.com>
2021-01-17 20:10:42 -0800

    initial commit

---
+++ python/challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -0,0 +1,11 @@
+  
+class InvalidOperationError(BaseException):
+    pass
+
+class AnimalShelter():
+  def __init__(self, node = None):  
+        self.top = node
+
+  # def enqueue():
+
+  # def dequeue():
